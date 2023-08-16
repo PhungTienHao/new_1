9bf7d27cb8a1782bca58533e763ef758a3368d2b
phuc <nguyenngocphuc2002bv@gmail.com>
2023-08-16 14:25:44 +0700

    hao

--- Backend/views/users/login.php
+++ Backend/views/users/login.php
@@ -1,19 +1,27 @@
 <?php
 //views/users/login.php
 ?>
+<body id="body">
+
 <div class="container">
-    <h2> from đăng Nhập</h2>
+    <h1 class="h-user">  đăng Nhập</h1>
     <form action="" method="post" >
+        <div class="form-group" >
+            <label for="username">username : </label>
+            <input type="text" id="username" name="username" class="form-control" >
+        </div>
         <div class="form-group">
-            <label for="username">username</label>
-            <input type="text" id="username" name="username" class="form-control"> </div>
-        <div class="form-group">
-            <label for="password">password</label>
-            <input type="password" id="password" name="password" class="form-control"> </div>
+            <label for="password">password :</label>
+            <input type="password" id="password" name="password" class="form-control" >
+        </div>
         <div class="form-group">
             <input type="checkbox" >Ghi Nhớ Đăng Nhập <br>
-            <input type="submit" name="submit" value="đăng ký" class="btn btn-success">
-            chưa có tài khoản , đăng ký tại <a href="index.php?controller=user&action=register">đây</a>
-        </div>
+
+            <input type="submit" name="submit" value="đăng nhập" class=" btn btn-success">
+            <br>
+             Nếu chưa có tài khoản , đăng ký  <a href="index.php?controller=user&action=register"> tại đây</a>
+
+</div>
     </form>
 </div>
+</body>
--- Backend/views/users/register.php
+++ Backend/views/users/register.php
@@ -3,37 +3,41 @@
 
 
 ?>
+   <body id="body">
 <div class="container">
-    <h2> from đăng ký</h2>
+    <h2 class="h-user"> from đăng ký</h2>
     <form action="" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="username">username</label>
-            <input type="text" id="username" name="username" class="form-control"> </div>
+            <input type="text" id="username" name="username" class="form-control">
+        </div>
         <div class="form-group">
             <label for="password">password</label>
-            <input type="password" id="password" name="password" class="form-control"> </div>
+            <input type="password" id="password" name="password" class="form-control">
+        </div>
         <div class="form-group">
             <label for="repas">nhập lại mật khẩu</label>
-            <input type="password" id="repas" name="repas" class="form-control"> </div>
+            <input type="password" id="repas" name="repas" class="form-control">
+        </div>
         <div class="form-group">
             <label for="name">nhập họ tên</label>
             <input type="text" id="name" name="name" class="form-control">
         </div>
         <div class="form-group">
-            <lable for="phone" > nhập số điện thoại</lable>
+            <label for="phone"> nhập số điện thoại</label>
             <input type="text" id="phone" name="phone" class="form-control">
         </div>
 
         <div class="form-group">
-            <lable for="address" > nhập địa chỉ</lable>
+            <label for="address" > nhập địa chỉ</label>
             <input type="text" id="address" name="address" class="form-control">
         </div>
         <div class="form-group">
-            <lable for="email" > nhập email </lable>
+            <label for="email" > nhập email </label>
             <input type="email" id="email" name="email" class="form-control">
         </div>
         <div class="form-group">
-            <lable for="avatar" >Chọn Ảnh Đại Diện của bạn</lable>
+            <label for="avatar" >Chọn Ảnh Đại Diện của bạn</label>
             <input type="file" id="avatar" name="avatar" class="form-control">
         </div>
 
@@ -42,5 +46,5 @@
         </div>
     </form>
 </div>
-
+  </body>
 
